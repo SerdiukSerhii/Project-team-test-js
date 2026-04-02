19dcaa58203b2c7f5c475b2f1e6f80861ef5b3c1
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-04-02 14:54:40 +0000

    Deploying to gh-pages from @ SerdiukSerhii/Project-team-test-js@c5bc933065b901d0a1fbd926d7e5b9b0eba0759c 🚀

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import{a}from"./assets/vendor-Dl2X3eg5.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();a.defaults.baseURL="https://furniture-store-v2.b.goit.study/api";async function f(){return(await a.get("/categories")).data}async function g(r="",t=1){const i={page:t,limit:8};return r&&r!=="Всі товари"&&(i.category=r),(await a.get("/furnitures",{params:i})).data}function y(r){return["Всі товари",...r].map(i=>`
+import{a as f}from"./assets/vendor-Dl2X3eg5.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))n(r);new MutationObserver(r=>{for(const s of r)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function e(r){const s={};return r.integrity&&(s.integrity=r.integrity),r.referrerPolicy&&(s.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?s.credentials="include":r.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(r){if(r.ep)return;r.ep=!0;const s=e(r);fetch(r.href,s)}})();f.defaults.baseURL="https://furniture-store-v2.b.goit.study/api";async function v(){return(await f.get("/categories")).data}async function b(i="",t=1){const e={page:t,limit:8};return i&&(e.category=i),(await f.get("/furnitures",{params:e})).data}function C(i){return[{name:"Всі товари",_id:""},...i].map(e=>`
         <li class="category-item">
-          <button class="category-btn" type="button" data-category="${i}">
-            ${i}
+          <button
+            class="category-btn"
+            type="button"
+            data-category="${e._id}">
+            ${e.name}
           </button>
         </li>
-      `).join("")}function p(r){return r.map(({_id:t,img:i,name:n,color:e,price:s})=>{const o=Array.isArray(e)?e:[e];return`
-        <li class="furniture-item" data-id="${t}">
+      `).join("")}function g(i){return i.map(t=>{const{_id:e,name:n,images:r=[],color:s=[],price:o=0}=t,p=r[0]||"placeholder.jpg",h=Array.isArray(s)?s:[s];return`
+        <li class="furniture-item" data-id="${e}">
           <div class="furniture-thumb">
-            <img src="${i}" alt="${n}" loading="lazy" />
+            <img src="${p}" alt="${n}" loading="lazy" />
           </div>
           <div class="furniture-info">
             <h3 class="furniture-heading">${n}</h3>
-             <ul class="furniture-color">
-              ${o.map(d=>`
+            <ul class="furniture-color">
+              ${h.map(L=>`
                 <li>
                   <svg width="24" height="24">
-                    <circle cx="12" cy="12" r="10" fill="${d}" />
+                    <circle cx="12" cy="12" r="10" fill="${L}" />
                   </svg>
                 </li>
               `).join("")}
             </ul>
-            <p class="furniture-price">${s} грн</p>
+            <p class="furniture-price">${o} грн</p>
             <button class="details-btn" type="button">Детальніше</button>
           </div>
         </li>
-      `}).join("")}const c=document.querySelector("#categories"),m=document.querySelector("#furniture-list"),u=document.querySelector("#loader");async function h(){try{u.classList.remove("hidden");const r=await f();c.innerHTML=y(r);const t=c.querySelector(".category-btn");t&&t.classList.add("is-active"),l("Всі товари")}finally{u.classList.add("hidden")}}async function l(r="",t=1){const i=await g(r,t);m.innerHTML=p(i.results),console.log(i)}function L(r){var i;const t=r.target.closest(".category-btn");t&&((i=document.querySelector(".category-btn.is-active"))==null||i.classList.remove("is-active"),t.classList.add("is-active"),l(t.dataset.category))}c.addEventListener("click",L);document.addEventListener("DOMContentLoaded",()=>{h()});//! ============= submit ======================================
-//! =========== Click on pagination ===========
+      `}).join("")}const d=document.querySelector("#categories"),u=document.querySelector("#furniture-list"),a=document.querySelector("#load-more"),l=document.querySelector("#loader");let y="",c=1;async function M(){try{l.classList.remove("hidden");const i=await v(),t=[{name:"Всі товари",_id:""},...i];d.innerHTML=C(t);const e=d.querySelector(".category-btn");e&&e.classList.add("is-active"),await m("",1)}finally{l.classList.add("hidden")}}async function m(i="",t=1){try{l.classList.remove("hidden"),y=i,c=t;const e=await b(i,t),n=(e==null?void 0:e.furnitures)||[];console.log("Items:",n),t===1?u.innerHTML=g(n):u.insertAdjacentHTML("beforeend",g(n)),!n.length||n.length<8?a.classList.add("hidden"):a.classList.remove("hidden")}catch(e){console.error("Помилка при завантаженні меблів:",e),u.innerHTML="<p>Щось пішло не так</p>",a.classList.add("hidden")}finally{l.classList.add("hidden")}}function S(i){var e;const t=i.target.closest(".category-btn");t&&((e=document.querySelector(".category-btn.is-active"))==null||e.classList.remove("is-active"),t.classList.add("is-active"),c=1,m(t.dataset.category,c))}d.addEventListener("click",S);a.addEventListener("click",()=>{c+=1,m(y,c)});document.addEventListener("DOMContentLoaded",()=>{M()});
 //# sourceMappingURL=index.js.map
