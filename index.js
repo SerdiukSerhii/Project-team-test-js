7100b1c87f3e7d0e08d8b243b441e6000b6f68fa
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-04-04 01:16:44 +0000

    Deploying to gh-pages from @ SerdiukSerhii/Project-team-test-js@881e6c79346635b0f50974e409213d343a9c9594 🚀

--- index.js
+++ index.js
@@ -14,18 +14,26 @@ import{a as f,i as l}from"./assets/vendor-ByN8xaf9.js";(function(){const e=docum
           </div>
           <div class="furniture-info">
             <h3 class="furniture-heading">${s}</h3>
-            <ul class="furniture-color">
-              ${L.map(b=>`
-                <li>
-                  <svg width="24" height="24">
-                    <circle cx="12" cy="12" r="12" fill="${b}" />
-                  </svg>
-                </li>
-              `).join("")}
-            </ul>
-            <p class="furniture-price">${c} грн</p>
-            <button class="details-btn" type="button">Детальніше</button>
+
+
+                <div class="container-colors">
+                  <ul class="furniture-color">
+                      ${L.map(b=>`
+                        <li>
+                          <svg width="24" height="24">
+                            <circle cx="12" cy="12" r="12" fill="${b}" />
+                          </svg>
+                        </li>
+                      `).join("")}
+                   </ul>
+               </div>
+                    <p class="furniture-price">${c} грн</p>
+
+
+
+
           </div>
+             <button class="details-btn" type="button">Детальніше</button>
         </li>
       `}).join("")}function S(r,e){r.insertAdjacentHTML("beforeend",g(e))}const n={categoriesList:document.querySelector("#categories"),furnitureList:document.querySelector("#furniture-list"),loadMoreBtn:document.querySelector("#load-more"),loader:document.querySelector("#loader")};let d="",a=1;const B=["Всі товари","М’які меблі","Шафи та системи зберігання","Ліжка та матраци","Столи","Стільці та табурети","Кухні","Меблі для дитячої","Меблі для офісу","Меблі для передпокою","Меблі для ванної кімнати","Садові та вуличні меблі","Декор та аксесуари"];async function $(){try{n.loader.classList.remove("hidden");const r=await v(),e=B.map(s=>{const i=r.find(o=>o.name.trim()===s.trim());return{_id:s==="Всі товари"?"":i?i._id:"temp-id",name:s}});n.categoriesList.innerHTML=C(e);const t=n.categoriesList.querySelector(".category-btn");t&&t.classList.add("is-active"),await m("",1)}catch(r){console.error(r)}finally{n.loader.classList.add("hidden")}}async function m(r="",e=1){try{y(),d=r,a=e;const t=await M(r,e),s=(t==null?void 0:t.furnitures)||[],i=(t==null?void 0:t.totalItems)||0,o=(t==null?void 0:t.limit)||8,c=Math.ceil(i/o);if(e===1){if(n.furnitureList.innerHTML="",s.length===0)return l.info({message:"Товарів не знайдено",position:"topRight"}),u(),t;n.furnitureList.innerHTML=g(s)}else S(n.furnitureList,s);return e>=c||s.length<o?u():R(),e>1&&(e>=c||s.length<o)&&l.info({message:"Ви досягли кінця списку",position:"topRight"}),t}catch{u(),n.furnitureList.innerHTML="",l.error({message:"Сталася помилка. Спробуйте пізніше",position:"topRight"})}finally{p()}}async function O(r){var t;const e=r.target.closest(".category-btn");e&&((t=document.querySelector(".category-btn.is-active"))==null||t.classList.remove("is-active"),e.classList.add("is-active"),d=e.dataset.category||"",a=1,await m(d,a))}n.categoriesList.addEventListener("click",O);n.loadMoreBtn.addEventListener("click",async()=>{a+=1,u(),y();try{await m(d,a);const r=document.querySelector(".furniture-item");if(r){const e=r.getBoundingClientRect().height;window.scrollBy({top:e*2,behavior:"smooth"})}}catch{l.error({message:"Сталася помилка. Спробуйте пізніше",position:"topRight"})}finally{p()}});function y(){n.loader.classList.add("is-visible")}function p(){n.loader.classList.remove("is-visible")}function R(){n.loadMoreBtn.classList.add("is-visible")}function u(){n.loadMoreBtn.classList.remove("is-visible")}document.addEventListener("DOMContentLoaded",()=>{$()});
 //# sourceMappingURL=index.js.map
